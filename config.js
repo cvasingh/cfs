6f718f5fb205ce4799fe473965e17de072f4a474
Shiva Singh <shivasingh.200087@gmail.com>
2022-06-26 14:07:44 +0530

    1.1.0

--- config.js
+++ config.js
@@ -2,13 +2,24 @@
 const config = {
 
   //database on localhost
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
+  // host: '127.0.0.1',
+  // user: 'root',
+  // database: 'tooltrackingdb',
+  // password: '',
+  // connectionLimit: 10,
+
+
+  
+  // //database on server
+  host: 'cfs.cnqdrdf0e76p.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
   database: 'tooltrackingdb',
+  password: '1234567890',
   connectionLimit: 10,
 
-  statusCode: { notExist: "NE", exist: "AE", notMatch: "NM", match: "M", inserted: "I", notInserted: "NI", error: "E", success: true, "failed": true }
+
+
+
 }
 
 module.exports = config
